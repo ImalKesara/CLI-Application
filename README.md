bc700b634d40e73b886ef946387811abe8ed36f2
ImalKesara <imalkesara2001@gmail.com>
2026-03-04 11:11:01 +0530

    Error handling it took 5-10 sec lol

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 A sample command-line application with an entrypoint in `bin/`, library code
 in `lib/`, and example unit test in `test/`.
+
+What are the differences of /lib and /lib/src
+lib/ -> public interface
+src/ -> private implementation
+
+concrete implementation
+a class / method that has the actual working code , not just a description of what it should do
+
+abstract = blueprint(what must exist)
+concrete = real working version(how it works)
+
--- bin/cli.dart
+++ bin/cli.dart
@@ -5,8 +5,21 @@ import 'package:command_runner/command_runner.dart';
 const version = "0.0.1";
 
 void main(List<String> arguments) async {
-  var runner = CommandRunner();
-  await runner.run(arguments);
+  // this equals to
+  // var commandRunner = CommandRunner();
+  // commandRunner.addCommand(HelpCommand());
+  var commandRunner = CommandRunner(
+    onError: (Object error) {
+      if (error is Error) {
+        throw error;
+      }
+      if (error is Exception) {
+        print(error);
+      }
+    },
+  )..addCommand(HelpCommand());
+
+  await commandRunner.run(arguments);
 }
 
 void printUsage() {
